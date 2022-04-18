e5cf50bb16a89f2ec3e5a5029f8585e83c3abae0
TheLast22 <christianstewart5111@gmail.com>
2022-04-18 08:56:19 +0800

    changed icon

--- ChatApp/ChatApp.Android/Properties/AssemblyInfo.cs
+++ ChatApp/ChatApp.Android/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ChatApp.Android")]
+[assembly: AssemblyTitle("Wave")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ChatApp.Android")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("Wave")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
