441b45567b00dd1484d3d346d0316f5cb4e372a3
therealmai <jomar35leano@gmail.com>
2022-04-18 11:51:12 +0800

    Splashscreen done

---
+++ ChatApp/ChatApp.Android/SplashActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChatApp.Droid
+{
+    [Activity(Label = "Wave", Icon = "@mipmap/ic_launcher_foreground", Theme = "@style/splashscreen", MainLauncher = true, NoHistory = true)]
+    class SplashActivity: AppCompatActivity
+    {
+        protected override void OnResume()
+        {
+            base.OnResume();
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
