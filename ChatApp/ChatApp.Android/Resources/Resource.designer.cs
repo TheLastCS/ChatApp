a589e18956e3c5bc1762be8ca79b4388af0a297d
TheLast22 <christianstewart5111@gmail.com>
2022-04-18 14:39:05 +0800

    added splash alt

--- ChatApp/ChatApp.Android/Resources/Resource.designer.cs
+++ ChatApp/ChatApp.Android/Resources/Resource.designer.cs
@@ -16667,13 +16667,16 @@ namespace ChatApp.Droid
 			public const int SplashScreen = 2131165327;
 			
 			// aapt resource value: 0x7F070090
-			public const int test_custom_background = 2131165328;
+			public const int SplashScreen_alt = 2131165328;
 			
 			// aapt resource value: 0x7F070091
-			public const int tooltip_frame_dark = 2131165329;
+			public const int test_custom_background = 2131165329;
 			
 			// aapt resource value: 0x7F070092
-			public const int tooltip_frame_light = 2131165330;
+			public const int tooltip_frame_dark = 2131165330;
+			
+			// aapt resource value: 0x7F070093
+			public const int tooltip_frame_light = 2131165331;
 			
 			static Drawable()
 			{
