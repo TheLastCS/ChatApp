3886fd9897ead655d42a538e05eb47bd734277fd
TheLast22 <christianstewart5111@gmail.com>
2022-04-19 11:59:36 +0800

    Added Login

---
+++ ChatApp/ChatApp/Pages/ProfilePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ChatApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProfilePage : ContentPage
+    {
+        public ProfilePage()
+        {
+            InitializeComponent();
+        }
+
+        public ProfilePage(string email)
+        {
+
+        }
+    }
+}
---
+++ ChatApp/ChatApp/Pages/TabbedPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+
+
+namespace ChatApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TabbedPage : Xamarin.Forms.TabbedPage
+    {
+        public TabbedPage()
+        {
+            InitializeComponent();
+            this.On<Xamarin.Forms.PlatformConfiguration.Android>().SetIsSwipePagingEnabled(false);
+            Xamarin.Forms.NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        public TabbedPage(string email)
+        {
+            InitializeComponent();
+            Xamarin.Forms.NavigationPage.SetHasNavigationBar(this, false);
+            email = email.Trim();
+            this.On<Xamarin.Forms.PlatformConfiguration.Android>().SetIsSwipePagingEnabled(false);
+
+        }
+    }
+}
