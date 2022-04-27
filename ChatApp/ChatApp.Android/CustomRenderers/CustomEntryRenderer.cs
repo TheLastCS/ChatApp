64dcea04dcad7192d642270c5713797a0e84d4d7
therealmai <jomar35leano@gmail.com>
2022-04-27 21:18:49 +0800

    custom entry partial

---
+++ ChatApp/ChatApp.Android/CustomRenderers/CustomEntryRenderer.cs
@@ -0,0 +1,45 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using ChatApp;
+using ChatApp.Droid.CustomRenderers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+
+namespace ChatApp.Droid.CustomRenderers
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        CustomEntry entry;
+        public CustomEntryRenderer(Context context) : base(context)
+        {
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            entry = Element as CustomEntry;
+        }
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+            var nativeEditText = (EditText)Control;
+            var shape = new ShapeDrawable(new Android.Graphics.Drawables.Shapes.RectShape());
+            shape.Paint.Color = entry.BorderColor.ToAndroid(); //entry.BorderColor is the 'BorderColor' property of the custom entry class
+            shape.Paint.SetStyle(Paint.Style.Stroke);
+            shape.Paint.StrokeWidth = 10;
+            nativeEditText.Background = shape;
+        }
+    }
+}
---
+++ ChatApp/ChatApp/CustomRenderers/CustomEntry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ChatApp
+{
+    public class CustomEntry : Entry
+    {
+        public static readonly BindableProperty BorderColorProperty = BindableProperty.Create(nameof(BorderColor), typeof(Color), typeof(CustomEntry), Color.Gray);
+        // Gets or sets BorderColor value  
+        public Color BorderColor
+        {
+            get => (Color)GetValue(BorderColorProperty);
+            set => SetValue(BorderColorProperty, value);
+        }
+
+        
+    }
+}
