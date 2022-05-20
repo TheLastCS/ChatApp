061229bb882df7d25c2fd210ae28f92c49806289
therealmai <jomar35leano@gmail.com>
2022-05-20 21:06:25 +0800

    Model done and made 2 helpers

---
+++ ChatApp/ChatApp/Helpers/isViewerConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ChatApp.Helpers
+{
+    public class isViewerConverter : IValueConverter
+    {
+        DataClass dataClass = DataClass.GetInstance;
+
+        public object Convert (object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool retval = false;
+
+            string[] players = value as string[];
+            if (!players[0].Equals(dataClass.loggedInUser.Id))
+                retval = true;
+
+            return retval;
+        }
+        public object ConvertBack (object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ChatApp/ChatApp/Models/MessageModel.cs
+++ ChatApp/ChatApp/Models/MessageModel.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using ChatApp.Core;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace ChatApp.Models
+namespace ChatApp
 {
-    public class MessageModel
+    public class MessageModel : ObservableObject
     {
-        public string Text { get; set; }
-        public string User { get; set; }
+        private string _id;
+        public string id
+        {
+            get { return _id; }
+            set { _id = value; OnPropertyChanged("id"); }
+        }
+
+        private string _message;
+        public string message
+        {
+            get { return _message; }
+            set { _message = value; OnPropertyChanged("message"); }
+        }
+
+        private string _converseeID;
+        public string converseeID
+        {
+            get { return _converseeID; }
+            set { _converseeID = value; OnPropertyChanged("converseeID"); }
+        }
+
+        private DateTime _created_at;
+        public DateTime created_at
+        {
+            get { return _created_at; }
+            set { _created_at = value; OnPropertyChanged("created_at"); }
+        }
     }
 }
