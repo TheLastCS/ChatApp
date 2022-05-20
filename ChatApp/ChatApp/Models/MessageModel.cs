7a3c13a5f8f7b1ae4eaca15c346eb924996fe7e6
TheLast22 <christianstewart5111@gmail.com>
2022-05-20 23:03:41 +0800

    Revert "Model done and made 2 helpers"
    This reverts commit 061229bb882df7d25c2fd210ae28f92c49806289.

--- ChatApp/ChatApp/Models/MessageModel.cs
+++ ChatApp/ChatApp/Models/MessageModel.cs
@@ -1,38 +1,12 @@
-﻿using ChatApp.Core;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace ChatApp
+namespace ChatApp.Models
 {
-    public class MessageModel : ObservableObject
+    public class MessageModel
     {
-        private string _id;
-        public string id
-        {
-            get { return _id; }
-            set { _id = value; OnPropertyChanged("id"); }
-        }
-
-        private string _message;
-        public string message
-        {
-            get { return _message; }
-            set { _message = value; OnPropertyChanged("message"); }
-        }
-
-        private string _converseeID;
-        public string converseeID
-        {
-            get { return _converseeID; }
-            set { _converseeID = value; OnPropertyChanged("converseeID"); }
-        }
-
-        private DateTime _created_at;
-        public DateTime created_at
-        {
-            get { return _created_at; }
-            set { _created_at = value; OnPropertyChanged("created_at"); }
-        }
+        public string Text { get; set; }
+        public string User { get; set; }
     }
 }
