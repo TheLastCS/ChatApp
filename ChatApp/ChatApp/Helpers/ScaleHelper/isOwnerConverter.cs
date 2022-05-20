f07903e8544d9dac4cd914c321b3a8d6f7e30179
therealmai <jomar35leano@gmail.com>
2022-05-21 00:50:53 +0800

    Helpers added

---
+++ ChatApp/ChatApp/Helpers/ScaleHelper/isOwnerConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ChatApp.Helpers.ScaleHelper
+{
+    public class isOwnerConverter : IValueConverter
+    {
+        DataClass dataClass = DataClass.GetInstance;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool retval = false;
+            if (value == null)
+            {
+                retval = false;
+            }
+
+            string[] persons = value as string[];
+
+            /* if the person is the logged in user */
+            if (persons[0].Equals(dataClass.loggedInUser.Id))
+                retval = true;
+
+            return retval;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ChatApp/ChatApp/Helpers/ScaleHelper/isViewerConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ChatApp.Helpers.ScaleHelper
+{
+    public class isViewerConverter : IValueConverter
+    {
+        DataClass dataclass = DataClass.GetInstance;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool retVal = false;
+            if (value != null)
+            {
+                ConversationModel conversation = value as ConversationModel;
+
+                if (conversation.converseeID.Equals(dataclass.loggedInUser.Id))
+                    retVal = true;
+            }
+            else
+            {
+                retVal = true;
+            }
+            return retVal;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
